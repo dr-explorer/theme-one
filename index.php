033bc85044d9a0ace3bece3432a067a50ee54926
dr-explorer <michael_scannell@hotmail.com>
2024-01-19 07:57:24 +0100

    Complete restoration of THeme One

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The fallback template file
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Theme One
+ * @since 1.0
+ */
+
+get_header();
+?>
+	<main>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'template-parts/content' ); ?>
+        <?php endwhile; endif; ?>
+    </main>
+<?php get_footer(); ?>
