6f072e2b5de5b4948bc3ac882fae08af0279695e
dr-explorer <michael_scannell@hotmail.com>
2024-02-05 15:25:43 +0100

    Corrected serious add_theme_support error

--- functions.php
+++ functions.php
@@ -18,7 +18,9 @@
 if ( ! function_exists( 'theme1_setup' )) {
     function theme1_setup() {
         load_theme_textdomain( 'theme-one', get_template_directory() . '/languages' );       
-        add_theme_support( 'title-tag', 'html5', 'post-thumbnails' );
+        add_theme_support( 'title-tag' );
+        add_theme_support( 'html5' );
+        add_theme_support( 'post-thumbnails' );
     }
     add_action( 'after_setup_theme', 'theme1_setup' );
 }
