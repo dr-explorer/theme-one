40d3fa0dcbccff9ad2ed4338609111112e04978a
dr-explorer <michael_scannell@hotmail.com>
2024-02-09 14:02:54 +0100

    Made it clear that explicit theme support for HTML5 is only a hint

--- functions.php
+++ functions.php
@@ -17,9 +17,10 @@
 
 if ( ! function_exists( 'theme1_setup' )) {
     function theme1_setup() {
-        load_theme_textdomain( 'theme-one', get_template_directory() . '/languages' );       
+        load_theme_textdomain( 'theme-one', get_template_directory() . '/languages' );
+        
         add_theme_support( 'title-tag' );
-        add_theme_support( 'html5' );
+        /*add_theme_support( 'html5', array() );*/
         add_theme_support( 'post-thumbnails' );
     }
     add_action( 'after_setup_theme', 'theme1_setup' );
