3543247608d64baa5cc425a6284c3acb063909b8
dr-explorer <michael_scannell@hotmail.com>
2024-02-09 13:58:52 +0100

    Broke sidebar into separate sections

--- starter-templates/sidebar.php
+++ starter-templates/sidebar.php
@@ -12,27 +12,33 @@
  * @since 1.0
  */
 ?>
-        <aside class="sidebar" id="site-content">
-            <h3>Latest Posts</h3>
-            <ul>
-                <?php
-                    $recent_posts = wp_get_recent_posts();
-                    foreach( $recent_posts as $recent ) {
-                        printf( '<li><a href="%1$s">%2$s</a></li>',
-                            esc_url( get_permalink( $recent['ID'] ) ),
-                            apply_filters( 'the_title', $recent['post_title'], $recent['ID'] )
-                        );
-                    }
-                ?>
-            </ul>
+        <aside class="sidebar" id="site-listings">
+            <section class="links" id="latest">
+                <h3>Latest Posts</h3>
+                <ul>
+                    <?php
+                        $recent_posts = wp_get_recent_posts();
+                        foreach( $recent_posts as $recent ) {
+                            printf( '<li><a href="%1$s">%2$s</a></li>',
+                                esc_url( get_permalink( $recent['ID'] ) ),
+                                apply_filters( 'the_title', $recent['post_title'], $recent['ID'] )
+                            );
+                        }
+                    ?>
+                </ul>
+            </section>
 
-            <h3>Categories</h3>
-              <ul>
-                <?php wp_list_categories('title_li='); ?>
-              </ul>
+            <section class="links" id="categories">
+                <h3>Categories</h3>
+                  <ul>
+                    <?php wp_list_categories('title_li='); ?>
+                  </ul>
+            </section>
 
-            <h3>Archives</h3>
-              <ul>
-                <?php wp_get_archives('type=monthly'); ?>
-              </ul>
+            <section class="links" id="archives">
+                <h3>Archives</h3>
+                  <ul>
+                    <?php wp_get_archives('type=monthly'); ?>
+                  </ul>
+            </section>
         </aside>
