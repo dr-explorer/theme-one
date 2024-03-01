8850a14b7684b6523b8dc9fb918214b0e78b61a4
dr-explorer <michael_scannell@hotmail.com>
2024-03-01 12:02:39 +0100

    Added the posts sidebar to the two posts pages

--- starter-templates/archive.php
+++ starter-templates/archive.php
@@ -13,18 +13,21 @@ get_header();
 ?>
 	<main>
         <div class="archive content">
-            <h2 class="page-title">
-            <?php if (is_day()) {
-                echo "Posts posted on ", get_the_date( 'jS F Y' );
-            } else if (is_month()) {
-                echo "Posts posted in ", get_the_date( 'F Y' );
-            } else if ( is_category() ) {
-                echo "Posts in the &lsquo;", single_cat_title(), "&rsquo; category";
-            } ?>
-            </h2>
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <?php get_template_part( 'template-parts/content', 'archive' ); ?>
-            <?php endwhile; endif; ?>
+            <article>
+                <h2 class="page-title">
+                <?php if (is_day()) {
+                    echo get_the_date( 'jS F Y' ), " posts";
+                } else if (is_month()) {
+                    echo get_the_date( 'F Y' ), " posts";
+                } else if ( is_category() ) {
+                    echo "&lsquo;", single_cat_title(), "&rsquo; posts";
+                } ?>
+                </h2>
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <?php get_template_part( 'template-parts/content', 'archive' ); ?>
+                <?php endwhile; endif; ?>
+            </article>
+            <?php get_sidebar(); ?>
         </div>
     </main>
 <?php get_footer(); ?>
--- starter-templates/home.php
+++ starter-templates/home.php
@@ -13,24 +13,27 @@ get_header();
 ?>
 	<main>
 	    <div class="post-page content">
-            <h2 class="page-title">Latest posts</h2>
-	                <?php if ( have_posts() ) : ?>
-	                <?php while ( have_posts() ) : the_post(); ?>
-	                    <?php get_template_part( 'template-parts/content', 'posts' ); ?>
-	            <?php endwhile; ?>
+            <article>
+            <h2 class="page-title">Posts</h2>
+                <?php if ( have_posts() ) : ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <?php get_template_part( 'template-parts/content', 'posts' ); ?>
+                    <?php endwhile; ?>
 	        
-	        <?php else: ?>
+	            <?php else: ?>
 	            
-	            <article class="error">
-	                <h3>Sorry, no posts were found</h3>
-	            </article>
+	                <article class="error">
+	                    <h3>Sorry, no posts were found</h3>
+	                </article>
 	        
-	        <?php endif; ?>
+    	        <?php endif; ?>
 	        
-	        <p class="post-page-navigation">
+    	        <p class="post-page-navigation">
 	            <?php next_posts_link( "&laquo; Previous posts"); ?>
 	            <?php previous_posts_link( "More recent posts &raquo;"); ?>
-	        </p>
+    	        </p>
+            </article>
+            <?php get_sidebar(); ?>
         </div>
     </main>
     <?php get_footer(); ?>
